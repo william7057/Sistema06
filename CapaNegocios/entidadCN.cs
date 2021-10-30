6e7d94d1f77ed3cf586caab61a26e1fa46dc7acc
william <factzure@hotmail.com>
2021-10-30 01:10:32 -0500

    Agregar archivos de proyecto.

---
+++ CapaNegocios/entidadCN.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocios
+{
+   public class entidadCN
+    {
+       
+            //Defi nición GLOBAL
+            Conexion objCon = new Conexion();
+            SqlConnection cn = new SqlConnection();
+            //Método que lista los contratistas
+            public DataSet listaUsuarios()
+            {
+                cn = objCon.getConecta();
+                SqlDataAdapter da = new SqlDataAdapter("select * from users", cn);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "Users");
+                return ds;
+            }
+        }
+}
---
+++ CapaPresentacion/frmListado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaNegocios;
+
+namespace CapaPresentacion
+{
+    public partial class frmListado : Form
+    {
+        //Defi nicion GLOBAL
+        entidadCN objL = new entidadCN();
+        public frmListado()
+        {
+            InitializeComponent();
+        }
+       
+        private void frmListado_Load(object sender, EventArgs e)
+        {
+            listaUser();
+
+        }
+        void listaUser()
+        {
+            dataGridView1.DataSource = objL.listaUsuarios().Tables["Users"];
+        }
+    }
+}
